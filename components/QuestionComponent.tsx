2bfd3b5e742585a2e46e6b1143cf91566c43d3f3
Mateusz Ptak <ptakm02@gmail.com>
2023-01-20 23:21:17 +0100

    questions: update stats tracker

--- components/QuestionComponent.tsx
+++ components/QuestionComponent.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Answer, Question } from '/types/question';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { StatsTrackerContext } from '/providers/StatsTrackerProvider';
 
 type FormInput = {
   [index: number]: boolean;
@@ -13,11 +14,19 @@ const QuestionComponent = ({ question, index }: { question: Question; index: num
     reset,
     formState: { isSubmitted },
   } = useForm();
+  const { handleCorrectAnswer, handleIncorrectAnswer } = useContext(StatsTrackerContext);
+
+  const onSubmit: SubmitHandler<FormInput> = (data: FormInput) => {
+    const answersState = question.answers.map((answer: Answer) => answer.isCorrect);
+    const selectedAnswers = Object.values(data);
+
+    JSON.stringify(answersState) === JSON.stringify(selectedAnswers) ? handleCorrectAnswer(index) : handleIncorrectAnswer(index);
+  };
 
   return (
     <div className="w-full flex flex-col gap-3 p-4 bg-stone-800 rounded-lg text-white shadow-xl">
       <h1 className="font-bold border-b-[1px] pb-2">{question.question}</h1>
-      <form onSubmit={handleSubmit(() => {})} id={question.question} className="flex items-start flex-col gap-3">
+      <form onSubmit={handleSubmit(onSubmit)} id={question.question} className="flex items-start flex-col gap-3">
         {question.answers.map((answer: Answer, idx: number) => (
           <div key={index + idx} className="w-full flex items-center justify-center gap-2 text-sm">
             <input {...register((index * 10 + idx).toString())} type="checkbox" id={(index * 10 + idx).toString()} className="hidden peer"></input>
--- providers/StatsTrackerProvider.tsx
+++ providers/StatsTrackerProvider.tsx
@@ -4,31 +4,39 @@ type AnswersContextType = {
   points: number;
   answeredQuestions: number;
   stats: string;
-  incrementPoints: () => void;
-  incrementAnsweredQuestions: () => void;
+  handleCorrectAnswer: (id: number) => void;
+  handleIncorrectAnswer: (id: number) => void;
 };
 
 export const StatsTrackerContext = createContext<AnswersContextType>({
   points: 0,
   answeredQuestions: 0,
   stats: '0/0',
-  incrementPoints: () => {},
-  incrementAnsweredQuestions: () => {},
+  handleCorrectAnswer: () => {},
+  handleIncorrectAnswer: () => {},
 });
 
 const StatsTrackerProvider = ({ children }: { children: ReactNode }) => {
   const [points, setPoints] = useState(0);
   const [answeredQuestions, setAnsweredQuestions] = useState(0);
   const [stats, setStats] = useState('0/0');
+  const [ids, setIds] = useState<Set<number>>(new Set());
 
-  const incrementPoints = () => {
-    setPoints((prev: number) => prev + 1);
-    setStats(`${points + 1}/${answeredQuestions}`);
+  const handleCorrectAnswer = (id: number) => {
+    if (!ids.has(id)) {
+      setStats(`${points + 1}/${answeredQuestions}`);
+      setPoints((prev: number) => prev + 1);
+      setAnsweredQuestions((prev: number) => prev + 1);
+      setIds((prev: Set<number>) => new Set(prev).add(id));
+    }
   };
 
-  const incrementAnsweredQuestions = () => {
-    setAnsweredQuestions((prev: number) => prev + 1);
-    setStats(`${points}/${answeredQuestions + 1}`);
+  const handleIncorrectAnswer = (id: number) => {
+    if (!ids.has(id)) {
+      setStats(`${points}/${answeredQuestions + 1}`);
+      setAnsweredQuestions((prev: number) => prev + 1);
+      setIds((prev: Set<number>) => new Set(prev).add(id));
+    }
   };
 
   return (
@@ -37,8 +45,8 @@ const StatsTrackerProvider = ({ children }: { children: ReactNode }) => {
         points,
         answeredQuestions,
         stats,
-        incrementPoints,
-        incrementAnsweredQuestions,
+        handleCorrectAnswer,
+        handleIncorrectAnswer,
       }}
     >
       {children}
