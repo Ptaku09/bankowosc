71beb1b6cf91cc1cbb156a938db419bca9c5ecde
Mateusz Ptak <ptakm02@gmail.com>
2023-01-20 19:21:16 +0100

    backend: remove api and fetch posts directly in getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import { Inter } from '@next/font/google';
-import { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import Question from '/types/question';
+import path from 'path';
+import { promises as fs } from 'fs';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const jsDirectory = path.join(process.cwd(), 'json');
+  const fileContents = await fs.readFile(jsDirectory + '/questions.json', 'utf8');
+  const questions = JSON.parse(fileContents);
+
+  return {
+    props: {
+      questions,
+    },
+  };
+};
+
+const Home = ({ questions }: { questions: Question[] }) => {
   return <h1 className={`text-3xl font-bold underline ${inter.className}`}>Hello world!</h1>;
 };
 
