f5219196092df30a3ba68b292c5f280e0c2d8f28
Mateusz Ptak <ptakm02@gmail.com>
2023-01-20 19:34:27 +0100

    questions: display questions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { Inter } from '@next/font/google';
+import { Raleway } from '@next/font/google';
 import { GetStaticProps } from 'next';
 import Question from '/types/question';
 import path from 'path';
 import { promises as fs } from 'fs';
 
-const inter = Inter({ subsets: ['latin'] });
+const raleway = Raleway({ weight: ['400', '700'], preload: false });
 
 export const getStaticProps: GetStaticProps = async () => {
   const jsDirectory = path.join(process.cwd(), 'json');
@@ -19,7 +19,18 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Home = ({ questions }: { questions: Question[] }) => {
-  return <h1 className={`text-3xl font-bold underline ${inter.className}`}>Hello world!</h1>;
+  return (
+    <div className={`flex items-center justify-start flex-col ${raleway.className}`}>
+      <div className="w-1/2">
+        {questions.map((question: Question, index: number) => (
+          <div key={index}>
+            <h1>{question.question}</h1>
+            <p>{question.answers[0].text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
