9a80e4bfd8f220ff92cb22fd78bc83c0e8a9ab89
Mateusz Ptak <ptakm02@gmail.com>
2023-02-03 23:35:24 +0100

    feat: add button for removing correctly answered questions from local storage

--- components/HideCorrectlyAnsweredToggle.tsx
+++ components/HideCorrectlyAnsweredToggle.tsx
@@ -16,7 +16,7 @@ const HideCorrectlyAnsweredToggle = () => {
       <label className="relative inline-flex items-center cursor-pointer">
         <input type="checkbox" value="" className="sr-only peer" {...register('hide-caq')} defaultChecked />
         <div className="w-11 h-6 bg-purple-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
-        <p className="ml-2">ukryj zal.</p>
+        <p className="ml-2 hidden md:block">ukryj zal.</p>
       </label>
     </div>
   );
