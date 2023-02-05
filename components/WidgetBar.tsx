4c03f6c778d51491c236e65ba58201032ddfb37a
Mateusz Ptak <ptakm02@gmail.com>
2023-02-05 22:05:31 +0100

    fix last question check button

--- components/WidgetBar.tsx
+++ components/WidgetBar.tsx
@@ -3,7 +3,7 @@ import StatsWidget from '/components/StatsWidget';
 
 const WidgetBar = () => {
   return (
-    <div className="w-full md:w-[500px] text-center fixed bottom-4 left-1/2 -translate-x-1/2 py-2 rounded-lg text-white font-bold flex justify-center gap-4">
+    <div className="w-44 text-center fixed bottom-4 left-1/2 -translate-x-1/2 py-2 rounded-lg text-white font-bold flex justify-center gap-4">
       <StatsWidget />
     </div>
   );
